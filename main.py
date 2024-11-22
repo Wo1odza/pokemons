78e72aab36ed64d361eaa63fbc2af8e3919c8db6
Wo1odza <yablochkin2010@gmail.com>
2024-11-22 23:57:08 +0500

    починил main

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import telebot 
+import telebot
 from config import token
-
 from logic import Pokemon
 
-bot = telebot.TeleBot(token) 
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['go'])
 def go(message):
-    if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
+    username = message.from_user.username
+    if username not in Pokemon.pokemons.keys():  #Изменено: сравниваем с общим словарем класса
+        pokemon = Pokemon(username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
     else:
